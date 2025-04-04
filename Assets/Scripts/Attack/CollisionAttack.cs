7a1844ce546ff91ff9b9ed8125f6b5aac415cb00
IsGris <homka228tyk@gmail.com>
2025-04-04 17:32:57 +0500

    Added braces
    Added braces in attack script if checks in attack function

--- Assets/Scripts/Attack/CollisionAttack.cs
+++ Assets/Scripts/Attack/CollisionAttack.cs
@@ -44,14 +44,16 @@ namespace Platformer
 
 			if (collision.collider.bounds.min.y + collisionAttackSettings.CollisionOffset/*Apply offset to prevent small deviations*/ 
                 > boxCollider.bounds.max.y) // check target bottom point higher than attacker top point
+            {
                 if (collisionAttackSettings.AttackTopCollision) // check does attack allowed 
 					ApplyAttack(targetHealth);
-
+            }
             else if (collision.collider.bounds.max.y - collisionAttackSettings.CollisionOffset/*Apply offset to prevent small deviations*/ 
                 < boxCollider.bounds.min.y) // check does target top point below attacker lowest point
+            {
                 if (collisionAttackSettings.AttackBottomCollision)  // check does attack allowed
 					ApplyAttack(targetHealth);
-
+            }
 			else if (collisionAttackSettings.AttackSideCollision) // if target not in upper and not in below position then it's on side
 				ApplyAttack(targetHealth);
 		}
